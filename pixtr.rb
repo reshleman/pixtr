c3f4e3b8670b8e3f2514636b97b8157903ed8403
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-06-25 17:57:41 -0400

    Implement ability to add new galleries via a web form

--- pixtr.rb
+++ pixtr.rb
@@ -19,10 +19,21 @@ get "/" do
   erb :index, layout: :layout
 end
 
+get "/galleries/new" do
+  @page_title = "Create a New Gallery"
+  erb :new_gallery, layout: :layout
+end
+
 get "/gallery/:name" do
   @name = params[:name]
   @gallery = Gallery.find_by(name: @name)
   @images = @gallery.images
   @page_title = "#{@gallery.name.capitalize} Gallery"
   erb :gallery, layout: :layout
+end
+
+post "/galleries" do
+  @gallery = Gallery.new(params[:gallery])
+  @gallery.save
+  redirect to("/gallery/#{@gallery.name}")
 end

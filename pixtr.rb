d2fe2aee97ed779427d71f8433368fd8348cd1b6
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-06-25 16:27:50 -0400

    Refactor to use database (w/ ActiveRecord) for images instead of a hash

--- pixtr.rb
+++ pixtr.rb
@@ -1,19 +1,28 @@
 require "sinatra"
+require "active_record"
 
-GALLERIES = {
-  "dogs" => ["shibe.png"],
-  "cats" => ["grumpy_cat.png", "colonel_meow.jpg"]
-}
+ActiveRecord::Base.establish_connection(
+  adapter: "postgresql",
+  database: "pixtr"
+)
+
+class Gallery < ActiveRecord::Base
+  has_many :images
+end
+
+class Image < ActiveRecord::Base
+end
 
 get "/" do
   @page_title = "Pixtr Galleries"
-  @galleries = GALLERIES.keys
+  @galleries = Gallery.order("name ASC")
   erb :index, layout: :layout
 end
 
 get "/gallery/:name" do
   @name = params[:name]
-  @filenames = GALLERIES[@name]
-  @page_title = "#{@name.capitalize} Gallery"
+  @gallery = Gallery.find_by(name: @name)
+  @images = @gallery.images
+  @page_title = "#{@gallery.name.capitalize} Gallery"
   erb :gallery, layout: :layout
 end

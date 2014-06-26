855eed257f5d31074c330a40c1ce2900d4d2ada1
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-06-26 15:34:08 -0400

    Reorder routes and add comments for clarity

--- pixtr.rb
+++ pixtr.rb
@@ -13,42 +13,50 @@ end
 class Image < ActiveRecord::Base
 end
 
+# GET index
 get "/" do
   @galleries = Gallery.order("name ASC")
   erb :index
 end
 
-get "/galleries/new" do
-  erb :new_gallery
-end
-
-post "/galleries" do
-  @gallery = Gallery.create(params[:gallery])
-  redirect to("/galleries/#{@gallery.id}")
-end
-
+# GET gallery instance
 get "/galleries/:gallery_id" do
   @gallery = Gallery.find(params[:gallery_id])
   @images = @gallery.images
   erb :gallery
 end
 
+# GET new gallery form
+get "/galleries/new" do
+  erb :new_gallery
+end
+
+# POST new gallery
+post "/galleries" do
+  @gallery = Gallery.create(params[:gallery])
+  redirect to("/galleries/#{@gallery.id}")
+end
+
+# GET edit gallery form
 get "/galleries/:gallery_id/edit" do
   @gallery = Gallery.find(params[:gallery_id])
   erb :edit_gallery
 end
 
+# PUT edited gallery
 put "/galleries/:gallery_id" do
   @gallery = Gallery.find(params[:gallery_id])
   @gallery.update(params[:gallery])
   redirect to("/galleries/#{@gallery.id}")
 end
 
+# GET new image form
 get "/galleries/:gallery_id/images/new" do
   @gallery_id = params[:gallery_id]
   erb :new_image
 end
 
+# POST new image
 post "/galleries/:gallery_id/images" do
   @image = Image.create(params[:image])
   redirect to("/galleries/#{@image.gallery_id}")
